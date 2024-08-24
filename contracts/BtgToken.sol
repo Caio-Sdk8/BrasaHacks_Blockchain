2a1dca4e17560f313f7212680be1a103a444300f
Caio-Sdk8 <caioadriana5@gmail.com>
2024-08-24 13:23:24 -0300

    first transaction

--- contracts/BtgToken.sol
+++ contracts/BtgToken.sol
@@ -3,9 +3,15 @@ pragma solidity 0.8.20;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 contract BTGToken is ERC20("Btg Token",  "BTG"), Ownable{
-    constructor() Ownable(msg.sender) {} // This line adds a new constructor that calls the base constructor of Ownable with msg.sender as argument
-    
+    constructor() Ownable(msg.sender) {} // This line adds a new constructor that calls the base constructor of Ownable with msg.sender as argument   
+
+    address to = 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db;
+                 
     function mintFifty() public onlyOwner {
-        _mint(msg.sender, 0);
+        _mint(msg.sender, 500);
+    }
+
+    function balanceOfAccount(address account) public view returns(uint) {
+        return balanceOf(account);
     }
 }
